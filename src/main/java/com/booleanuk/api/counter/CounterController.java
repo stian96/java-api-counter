0537f201dc584cd836f5d66ac1a651b636b78ba6
Stian Rusvik <stian.rusvik@no.experis.com>
2024-08-28 09:27:07 +0200

    fixed path mapping.

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class CounterController {
     private Counter counter = new Counter();
 
-    @GetMapping("/")
+    @GetMapping("/counter")
     public String showCounter() {
         return "Initial value of the counter: " + counter.getNumber();
     }
 
-    @GetMapping("/increment")
+    @GetMapping("/counter/increment")
     public String incrementCounter() {
         this.counter.increment();
         return "Incremented counter, new value is: " + this.counter.getNumber();
     }
 
-    @GetMapping("/decrement")
+    @GetMapping("/counter/decrement")
     public String decrementCounter() {
         this.counter.decrement();
         return "Decremented counter, new values is: " + this.counter.getNumber();
